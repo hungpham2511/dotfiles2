21ce295ed43fb321009117bb033cdc6bd0e5a756
hung <hungpham2511@gmail.com>
2018-11-29 14:14:26 +0800

    awesome

--- dotfiles_install.sh
+++ dotfiles_install.sh
@@ -1,4 +1,10 @@
 # install stuffs
+sudo add-apt-repository ppa:klaus-vormweg/awesome
+sudo apt-get update
+
+# awesomewm
+sudo apt install -y awesome awesome-extra
+
 # zsh
 sudo apt install git-core zsh -y
 chsh -s $(which zsh)
