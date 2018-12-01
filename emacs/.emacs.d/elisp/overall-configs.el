b4029ce51294a4578ff472c96ecb8aa5c8109e22
hung <hungpham2511@gmail.com>
2018-12-01 17:47:25 +0800

    new font

--- emacs/.emacs.d/elisp/overall-configs.el
+++ emacs/.emacs.d/elisp/overall-configs.el
@@ -7,8 +7,8 @@
 (tool-bar-mode -1)			; no tool bar with icons
 (scroll-bar-mode -1)			; no scroll bars
 (menu-bar-mode -1)
-;; (set-face-font 'default "Monospace-11") ; default font
-(set-face-font 'default "Inconsolata-14") ; default font
+(set-face-font 'default "Monospace-11") ; default font
+;; (set-face-font 'default "Inconsolata-14") ; default font
 (global-hl-line-mode)			; highlight current line
 (global-set-key (kbd "C-c e") 'eval-region)
 
